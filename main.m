f2eaf74babaa65d6c0e9dd07fe0ee85c90717bdb
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2025-05-04 14:14:49 -0400

    yippie

--- main.m
+++ main.m
@@ -35,7 +35,7 @@ end
 f_new = f; % Update variable
 f_eq = f; % Equilibrium
 
-timer = 500;
+timer = 5000;
 
 %% Solving
 
@@ -45,10 +45,10 @@ for t=1:timer
         for i = 1:N_x
             if j == 1 % top boundary
                 if i == 1 % topleft
-                    f_new(1,j,i) = f(1, j, i);
-                    f_new(3,j,i) = f(3, j+1, i);
-                    f_new(4,j,i) = f(4, j, i+1);
-                    f_new(7,j,i) = f(7, j+1, i+1);
+                    f_new(1,j,i) = f(1,j,i);
+                    f_new(3,j,i) = f(3,j+1,i);
+                    f_new(4,j,i) = f(4,j,i+1);
+                    f_new(7,j,i) = f(7,j+1,i+1);
                     % Abnormal
                     f_new(2,j,i) = f_new(4,j,i);
                     f_new(5,j,i) = f_new(3,j,i);
@@ -133,9 +133,9 @@ for t=1:timer
                 f_new(6,j,i) = f(6, j+1, i-1);
                 f_new(9,j,i) = f(9, j-1, i-1);
                 % Abnormal
-                f_new(4,j,i) = f(4, j, i-1);
-                f_new(7,j,i) = f(7, j, i-1);
-                f_new(8,j,i) = f(8, j, i-1);
+                f_new(4,j,i) = f_new(4, j, i-1);
+                f_new(7,j,i) = f_new(7, j, i-1);
+                f_new(8,j,i) = f_new(8, j, i-1);
             else % interior
                 f_new(1,j,i) = f(1, j, i);
                 f_new(2,j,i) = f(2, j, i-1);
@@ -149,122 +149,6 @@ for t=1:timer
             end
         end
     end
-% for j=1:N_y
-%     for i=1:N_x
-%         if j == 1 % This is the top boundary nodes
-%             if i ==1 % Top-Left corner node
-%                 f_new(1,j,i) = f(1,j,i);
-%                 f_new(3,j,i) = f(3,j+1,i);
-%                 f_new(4,j,i) = f(4,j,i+1);
-%                 f_new(7,j,i) = f(7,j+1,i+1);
-% 
-%                 % Unknown
-%                 f_new(2,j,i) = f_new(4,j,i);
-%                 f_new(5,j,i) = f_new(3,j,i);
-%                 f_new(6,j,i) = Rho_in/2 - f_new(1,j,i)/2 - f_new(3,j,i) - f_new(4,j,i) - f_new(7,j,i);
-%                 f_new(8,j,i) = f_new(6,j,i);
-%                 f_new(9,j,i) = f_new(7,j,i);
-%             elseif i == N_x % Top-right corner node
-%                 f_new(1,j,i) = f(1,j,i);
-%                 f_new(2,j,i) = f(2,j,i-1);
-%                 f_new(3,j,i) = f(3,j+1,i);
-%                 f_new(6,j,i) = f(6,j+1,i-1);
-% 
-%                 % Unknown
-%                 f_new(4,j,i) = f_new(2,j,i);
-%                 f_new(5,j,i) = f_new(3,j,i);
-%                 f_new(7,j,i) = f_new(7,j,i-1);
-%                 f_new(8,j,i) = f_new(6,j,i);
-%                 f_new(9,j,i) = f_new(7,j,i);
-%             else % All other nodes on the top boundary
-%                 f_new(1,j,i) = f(1,j,i);
-%                 f_new(2,j,i) = f(2,j,i-1);
-%                 f_new(3,j,i) = f(3,j+1,i);
-%                 f_new(4,j,i) = f(4,j,i+1);
-%                 f_new(6,j,i) = f(6,j+1,i-1);
-%                 f_new(7,j,i) = f(7,j+1,i+1);
-% 
-%                 % Unknown
-%                 f_new(5,j,i) = f_new(3,j,i);
-%                 f_new(8,j,i) = f_new(6,j,i) + (f_new(2,j,i)-f_new(4,j,i))/2;
-%                 f_new(9,j,i) = f_new(7,j,i) - (f_new(2,j,i)-f_new(4,j,i))/2;
-%             end
-%         elseif j == N_y % This is the bottom boundary nodes
-%             if i ==1 % Bottom-Left corner node
-%                 f_new(1,j,i) = f(1,j,i);
-%                 f_new(4,j,i) = f(4,j,i+1);
-%                 f_new(5,j,i) = f(5,j-1,i);
-%                 f_new(8,j,i) = f(8,j-1,i+1);
-% 
-%                 % Unknown
-%                 f_new(2,j,i) = f_new(4,j,i);
-%                 f_new(3,j,i) = f_new(5,j,i);
-%                 f_new(6,j,i) = f_new(8,j,i);
-%                 f_new(7,j,i) = Rho_in/2 -f_new(1,j,i)/2 -f_new(4,j,i) - f_new(5,j,i) - f_new(8,j,i);
-%                 f_new(9,j,i) = f_new(7,j,i);
-%             elseif i == N_x % Bottom-right corner node
-%                 f_new(1,j,i) = f(1,j,i);
-%                 f_new(2,j,i) = f(2,j,i-1);
-%                 f_new(5,j,i) = f(5,j-1,i);
-%                 f_new(9,j,i) = f(9,j-1,i-1);
-% 
-%                 % Unknown
-%                 f_new(3,j,i) = f_new(5,j,i);
-%                 f_new(4,j,i) = f_new(2,j,i);
-%                 f_new(6,j,i) = f_new(6,j,i-1);
-%                 f_new(7,j,i) = f_new(9,j,i);
-%                 f_new(8,j,i) = f_new(6,j,i);
-%             else % All other nodes on the bottom boundary
-%                 f_new(1,j,i) = f(1,j,i);
-%                 f_new(2,j,i) = f(2,j,i-1);
-%                 f_new(4,j,i) = f(4,j,i+1);
-%                 f_new(5,j,i) = f(5,j-1,i);
-%                 f_new(8,j,i) = f(8,j-1,i+1);
-%                 f_new(9,j,i) = f(9,j-1,i-1);
-% 
-%                 % Unknown
-%                 f_new(3,j,i) = f_new(5,j,i);
-%                 f_new(6,j,i) = f_new(8,j,i) + (f_new(4,j,i)-f_new(2,j,i))/2;
-%                 f_new(7,j,i) = f_new(9,j,i) - (f_new(4,j,i)-f_new(2,j,i))/2;
-%             end
-%         elseif i == 1 % This is the left boundary nodes
-%             f_new(1,j,i) = f(1,j,i);
-%             f_new(3,j,i) = f(3,j+1,i);
-%             f_new(4,j,i) = f(4,j,i+1);
-%             f_new(5,j,i) = f(5,j-1,i);
-%             f_new(7,j,i) = f(7,j+1,i+1);
-%             f_new(8,j,i) = f(8,j-1,i+1);
-% 
-%             % Unknown
-%             U_in = 1-(f_new(1,j,i)+f_new(3,j,i)+f_new(5,j,i)+2*(f_new(4,j,i)+f_new(7,j,i)+f_new(8,j,i)))/Rho_in;
-%             f_new(2,j,i) = f_new(4,j,i)+Rho_in*U_in*2/3;
-%             f_new(6,j,i) = f_new(8,j,i)+(f_new(5,j,i)-f_new(3,j,i))/2+Rho_in*U_in/6; % Double Check
-%             f_new(9,j,i) = f_new(7,j,i)-(f_new(5,j,i)-f_new(3,j,i))/2+Rho_in*U_in/6; % Double Check
-%         elseif i == N_x % This is the right boundary nodes
-%             f_new(1,j,i) = f(1,j,i);
-%             f_new(2,j,i) = f(2,j,i-1);
-%             f_new(3,j,i) = f(3,j+1,i);
-%             f_new(5,j,i) = f(5,j-1,i);
-%             f_new(6,j,i) = f(6,j+1,i-1);
-%             f_new(9,j,i) = f(9,j-1,i-1);
-% 
-%             % Unknown
-%             f_new(4,j,i) = f_new(4,j,i-1);
-%             f_new(7,j,i) = f_new(7,j,i-1);
-%             f_new(8,j,i) = f_new(8,j,i-1);
-%         else  % All interior nodes
-%             f_new(1,j,i) = f(1,j,i);
-%             f_new(2,j,i) = f(2,j,i-1);
-%             f_new(3,j,i) = f(3,j+1,i);
-%             f_new(4,j,i) = f(4,j,i+1);
-%             f_new(5,j,i) = f(5,j-1,i);
-%             f_new(6,j,i) = f(6,j+1,i-1);
-%             f_new(7,j,i) = f(7,j+1,i+1);
-%             f_new(8,j,i) = f(8,j-1,i+1);
-%             f_new(9,j,i) = f(9,j-1,i-1);
-%         end
-%     end
-% end
     % Collision
     % Rho, U calculation
     for j = 1:N_y
